6a1fa540f80a4532f51121366631baebbaf3e053
marcos.goedert <marcosgoedert15@gmail.com>
2022-05-15 19:43:10 -0300

    Criando funcionalidade de chamar jogos

---
+++ jogos.py
@@ -0,0 +1,19 @@
+import adivinhacao
+import forca
+
+print("*********************************")
+print("*******Escolha o seu jogo!*******")
+print("*********************************")
+
+print("(1) Forca (2) Adivinhação")
+
+jogo = int(input("Qual jogo?"))
+
+if(jogo == 1):
+    print("Jogando Forca")
+    forca.jogar()
+elif(jogo == 2):
+    print("Jogando Adivinhação")
+    adivinhacao.jogar()
+else:
+    print("Jogo não encontrado!")
